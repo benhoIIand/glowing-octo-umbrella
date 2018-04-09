e39c4ec048f694eafebd66816f67ee92b0e02200
Ben Holland <benholland99@gmail.com>
2018-04-09 18:48:08 +0100

    Implement job formatter

--- tests/job-formatter.spec.js
+++ tests/job-formatter.spec.js
@@ -19,41 +19,51 @@ const jobFormatter = require('../src/job-formatter');
 
 describe('When formatting a job', () => {
 
-    it('should return the next time it will run', () => {
-      const scenarios = [
-        {
-          job: { minute: 30, hour: 1, cmd: '/bin/cmd' },
-          currentTime: '16:10',
-          expected: '1:30 tomorrow - /bin/cmd',
-        },
-        {
-          job: { minute: 45, hour: '*', cmd: '/bin/cmd' },
-          currentTime: '16:10',
-          expected: '16:45 today - /bin/cmd',
-        },
-        {
-          job: { minute: '*', hour: '*', cmd: '/bin/cmd' },
-          currentTime: '16:10',
-          expected: '16:10 today - /bin/cmd',
-        },
-        {
-          job: { minute: '*', hour: 19, cmd: '/bin/cmd' },
-          currentTime: '16:10',
-          expected: '19:00 today - /bin/cmd',
-        },
-        {
-          job: { minute: 0, hour: '*', cmd: '/bin/cmd' },
-          currentTime: '16:10',
-          expected: '15:00 today - /bin/cmd',
-        },
-        {
-          job: { minute: 30, hour: '*', cmd: '/bin/cmd' },
-          currentTime: '00:00',
-          expected: '00:30 today - /bin/cmd',
-        },
-      ];
+    const scenarios = [
+      {
+        job: { minute: 30, hour: 1, cmd: '/bin/cmd' },
+        currentTime: '16:10',
+        expected: '01:30 tomorrow - /bin/cmd',
+      },
+      {
+        job: { minute: '*', hour: 1, cmd: '/bin/cmd' },
+        currentTime: '16:10',
+        expected: '01:00 tomorrow - /bin/cmd',
+      },
+      {
+        job: { minute: 45, hour: '*', cmd: '/bin/cmd' },
+        currentTime: '16:10',
+        expected: '16:45 today - /bin/cmd',
+      },
+      {
+        job: { minute: '*', hour: '*', cmd: '/bin/cmd' },
+        currentTime: '16:10',
+        expected: '16:10 today - /bin/cmd',
+      },
+      {
+        job: { minute: '*', hour: 19, cmd: '/bin/cmd' },
+        currentTime: '16:10',
+        expected: '19:00 today - /bin/cmd',
+      },
+      {
+        job: { minute: 0, hour: '*', cmd: '/bin/cmd' },
+        currentTime: '16:10',
+        expected: '17:00 today - /bin/cmd',
+      },
+      {
+        job: { minute: 30, hour: '*', cmd: '/bin/cmd' },
+        currentTime: '00:00',
+        expected: '00:30 today - /bin/cmd',
+      },
+      {
+        job: { minute: '*', hour: '*', cmd: '/bin/cmd' },
+        currentTime: '16:10',
+        expected: '16:10 today - /bin/cmd',
+      },
+    ];
 
-      scenarios.forEach(({ job, currentTime, expected }) => {
+    scenarios.forEach(({ job, currentTime, expected }) => {
+      it('should return the next time it will run', () => {
         const res = jobFormatter(job, currentTime);
 
         expect(res).toEqual(expected);
